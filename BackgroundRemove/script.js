e1285cac22104f69adf002da641315686e415e43
PrachiCKadu <140785871+PrachiCKadu@users.noreply.github.com>
2023-11-26 18:40:10 +0530

    Add files via upload

---
+++ BackgroundRemove/script.js
@@ -0,0 +1,54 @@
+let profilePic = document.getElementById("profile-pic");
+let fileInput = document.getElementById("fileInput");
+let imageURL;
+
+fileInput.onchange = function(){
+    profilePic.src = URL.createObjectURL(fileInput.files[0]);
+}
+
+function submitHandler(){
+    // console.log("click");
+const fileInput = document.getElementById('fileInput');
+    // console.log(fileInput.files);
+    const image = fileInput.files[0];
+
+    // Multipart file
+    const formData = new FormData();
+    formData.append('image_file', image);
+    formData.append('size', 'auto');
+
+    const apiKey = "tf1EEjVBDFRn5DFHxbTgHfTG";
+
+    fetch('https://api.remove.bg/v1.0/removebg',{
+        method:'POST',
+        headers: {
+        'X-Api-Key': apiKey
+     },
+     body: formData
+    })
+    .then(function(reponse){
+            return reponse.blob()
+    })
+    .then(function(blob){
+            // console.log(blob);
+            const url = URL.createObjectURL(blob);
+            imageURL = url;
+            // const img = document.createElement('img');
+            profilePic.src = url;
+            // document.body.appendChild(img);
+    })
+    .catch(function(error){
+        console.error(error);
+    });
+}
+
+function downloadFile(){
+var a = document.createElement('a'); //<a></a>
+    a.href = imageURL;
+    a.download = 'naciasv.png';
+    document.body.appendChild(a);
+
+    a.click();
+
+    document.body.removeChild(a);
+}
